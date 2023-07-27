fdb5540745db10c41eb2e30c1e4e1e388bd93cc5
SalsaAddict <pierre.henry@salsaaddict.com>
2023-07-27 14:37:45 +0100

    ng@16

--- src/app/song/song.resolver.ts
+++ src/app/song/song.resolver.ts
@@ -1,16 +1,12 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import {
-  Resolve,
-  RouterStateSnapshot,
-  ActivatedRouteSnapshot,
-} from "@angular/router";
+import { RouterStateSnapshot, ActivatedRouteSnapshot } from "@angular/router";
 import { Observable } from "rxjs";
 import { ISongs, IBreakdown } from "src/schema/schema";
 import { Song } from "./song";
 
 @Injectable({ providedIn: "root" })
-export class SongResolver implements Resolve<Song> {
+export class SongResolver  {
   constructor(private readonly http: HttpClient) {}
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
     return new Observable<Song>((observer) => {
